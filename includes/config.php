0eae45232ecb0f520e6169dbc2e8fe5d1317e4f5
AmilcarArmmand <armmand@gmail.com>
2025-07-03 01:29:53 +0000

    moved files

---
+++ includes/config.php
@@ -0,0 +1,13 @@
+<?php
+/* database configuration */
+define('DB_HOST', 'localhost');
+define('DB_USER', 'admin');
+define('DB_PASS', 'adminpass');
+define('DB_NAME', 'amm_games_db');
+
+// Start session
+session_start();
+// Error reporting
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+?>
